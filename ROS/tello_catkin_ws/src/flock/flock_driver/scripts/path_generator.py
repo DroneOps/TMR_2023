30d3a90f079f70dd3759e59574250dc102e2075c
Pablo Ortega Kral <2001.paok@gmail.com>
2023-04-18 15:51:21 -0600

    Added new trajectory node for 3D mapping mission

---
+++ ROS/tello_catkin_ws/src/flock/flock_driver/scripts/path_generator.py
@@ -0,0 +1,19 @@
+import os
+import math
+import csv
+import numpy as np  
+
+def points_circle(radius,points, height_max, height_min):
+    return [(-1*round(math.cos(2*math.pi / points*x)*radius,2)+radius,
+             -1*round(math.sin(2*math.pi / points*x)*radius,2),
+             round((math.cos(x*0.5*math.pi)+1)*0.5*(height_max-height_min)+height_min,2),
+             90-1*round(np.rad2deg(math.atan2(math.cos(2*math.pi / points*x)*radius,math.sin(2*math.pi / points*x)*radius)),2)) 
+             for x in range(0,points+1)]
+
+def write2csv(pointvec):
+    path_file = open('/home/droneops/Documents/TMR_2023/ROS/tello_catkin_ws/src/flock/flock_driver/src/mapper.csv', 'w')
+    writer = csv.writer(path_file)
+    for coord in pointvec:
+        writer.writerow(coord)
+
+write2csv(points_circle(1,20,1,0.3))
